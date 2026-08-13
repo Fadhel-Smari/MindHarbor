d9f62fe20aaed8e8e54d12a4d875b9cc611cb0d0
Gabriel Cadieux <gabrielcadieux96@gmail.com>
2026-08-13 16:58:48 -0400

    Ajout de src et server.ts

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+
+app.use(cors())
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Serveur sur le http://localhost:${PORT}/`)
+})
